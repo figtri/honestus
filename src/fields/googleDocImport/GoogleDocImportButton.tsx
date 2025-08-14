ded6a08cb99c924fe53be06acbe2fd6f4184811e
salif <tanksalif@gmail.com>
2025-08-14 12:01:06 -0400

    feat: add Google Doc import button component

---
+++ src/fields/googleDocImport/GoogleDocImportButton.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import React from 'react'
+import { Button } from '@payloadcms/ui'
+
+export const GoogleDocImportButton: React.FC = () => {
+  return (
+    <div className="google-doc-import-button">
+      <Button 
+        buttonStyle="secondary"
+        size="small"
+        disabled
+      >
+        Import from Google Doc
+      </Button>
+      <p className="text-xs text-gray-500 mt-1">
+        Paste a Google Doc URL above and save to import content
+      </p>
+    </div>
+  )
+}
